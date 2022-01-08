bc3e6bbb305e6716165928fc52f01b763957190f
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2022-01-08 08:36:30 +0700

    Add files via upload

---
+++ Pattern_starts_oneone.java
@@ -0,0 +1,16 @@
+package forloop.workings;
+
+public class Pattern_starts_oneone {
+	public static void main(String[] args)
+	{
+		for(int j=1;j<=5;j++) 
+		{
+		for(int i=j;i<=5;i++)
+		{
+			System.out.print(j+" ");
+		}
+		System.out.println();
+	}
+
+}
+}
