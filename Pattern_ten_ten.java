1ef2aaef0cd305b36f31dd10376ac3dace9e75fd
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2022-01-08 08:36:14 +0700

    Add files via upload

---
+++ Pattern_ten_ten.java
@@ -0,0 +1,19 @@
+package forloop.workings;
+
+public class Pattern_ten_ten {
+	public static void main(String[] args)
+	{
+		for(int j=1;j<=10;j++)
+		{
+			if(j%2==0)
+				continue;
+		for(int i=j;i<=10;i++)
+		{
+			
+			System.out.print(j+" ");
+		}
+		System.out.println();
+	}
+
+}
+}
